e99a2e1b517632d441d97777b22b18c60d9dcf6c
Jonathan SAHM <contact@johnstyle.fr>
2014-01-29 23:48:07 +0100

    Mep fichiers parts

---
+++ app/View/parts/bottom-navbar.php
@@ -0,0 +1,9 @@
+<footer class="navbar navbar-default navbar-fixed-bottom" role="navigation">
+    <div class="container">
+        <div class="collapse navbar-collapse">
+            <p id="credits" class="navbar-text navbar-right">
+                <a href="https://github.com/johnstyle/sjo-app" target="_blank"><strong>sJo</strong> PHP Framework</a>
+            </p>
+        </div>
+    </div>
+</footer>
