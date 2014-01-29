3046a908e2ebecb0d5dbe5a52a198bb0aa391b16
Jonathan SAHM <contact@johnstyle.fr>
2014-01-29 23:50:16 +0100

    include parts/top-navbar

--- app/View/header.php
+++ app/View/header.php
@@ -1,6 +1,13 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>sJo App</title>
-    </head>
-    <body>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>sJo App</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="stylesheet" href="libs/bootstrap/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <link href='http://fonts.googleapis.com/css?family=Marcellus+SC' rel='stylesheet' type='text/css'>
+</head>
+<body>
+    <div id="wrap">
+        <?php self::inc('parts/top-navbar'); ?>
