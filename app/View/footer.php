bf179f1c33691e6fe41c93d1b2a0eabd6676b60e
Jonathan SAHM <contact@johnstyle.fr>
2014-01-29 23:49:59 +0100

    Include parts/bottom-navbar

--- app/View/footer.php
+++ app/View/footer.php
@@ -1,2 +1,8 @@
-    </body>
+    </div>
+
+    <?php self::inc('parts/bottom-navbar'); ?>
+
+    <script src="libs/jquery/js/jquery.min.js" type="text/javascript"></script>
+    <script src="libs/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+</body>
 </html>
